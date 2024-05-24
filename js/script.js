e610f57695a02df089e858327633d4b864ef0d1e
tiaxgo <tiiagoalmeiida1995@hotmail.com>
2024-05-24 19:35:59 -0300

    menu lateral

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ changeThemeBtn.addEventListener("change", function() {
     document.body.classList.toggle("dark");
 
 })
+
+/* abaixo menu com overlay*/ 
+
+let btnMenu = document.getElementById('btn-abrir-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})

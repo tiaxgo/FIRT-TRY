9b367512a42b169ca5ef11cf4f9567625033d397
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2024-11-21 15:29:16 -0300

    Up

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ changeThemeBtn.addEventListener("change", function() {
 
 })
 
-/* abaixo menu com overlay versap mobile*/ 
+/* abaixo menu com overlay versao mobile*/ 
 
 let btnMenu = document.getElementById('btn-abrir-menu')
 let menu = document.getElementById('menu-mobile')
